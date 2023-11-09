7268cbd87717a5d62594cf5cd8b6ff876eec1afb
Jack Donohoe <20093738@mail.wit.ie>
2023-11-09 15:44:34 +0000

    Copied over cognito-lab as base, implemented movieReview type and created reviews database

---
+++ seed/reviews.ts
@@ -0,0 +1,10 @@
+import { MovieReview } from "../shared/types";
+
+export const revies: MovieReview[] = [
+    {
+        movieId : 1,
+        reviewerName : "Jack Donohoe",
+        reviewDate : "09/11/2023",
+        content : "Movie is sufficient"
+    },
+]
